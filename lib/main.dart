ab4a3fa00d10557360edad0bde23798a3e1744a8
Aybak3k <nooreldin3000@gmail.com>
2022-05-03 21:40:01 +0200

    format && recreate

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+// imports
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+// Vars
+const Color mainColor = Colors.red; // prefer const over final
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  Widget imageButton(int diceNumber) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: mainColor,
+        appBar: AppBar(
+          title: const Text("Who's It?"),
+          backgroundColor: mainColor,
+        ),
+        body: Expanded(
+          child: TextButton(
+            onPressed: () {
+              changeDiceFace();
+            },
+            child: Image.asset("images/dice$diceNumber.png"),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          imageButton(leftDiceNumber),
+          imageButton(rightDiceNumber),
+        ],
+      ),
+    );
+  }
+}
